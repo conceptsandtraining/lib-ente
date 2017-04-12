32abfc9e7c2856799f9fae502cf93636795e393b
Richard Klees <richard.klees@concepts-and-training.de>
2017-04-12 17:30:55 +0200

    added another component for testing.

---
+++ src/Simple/AttachIntMemory.php
@@ -0,0 +1,46 @@
+<?php
+/******************************************************************************
+ * An entity component framework for PHP.
+ *
+ * Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de>
+ *
+ * This software is licensed under GPLv3. You should have received a copy of
+ * the license along with the code.
+ */
+
+namespace CaT\Ente\Simple;
+
+/**
+ * In memory implementation of AttachInt.
+ */
+class AttachIntMemory implements AttachInt {
+    /**
+     * @var \CaT\Ente\Component
+     */
+    private $entity;
+
+    /**
+     * @var int 
+     */
+    private $attached_int;
+
+    public function __construct(Entity $entity, $attached_int) {
+        assert('is_int($attached_int)');
+        $this->entity = $entity;
+        $this->attached_int = $attached_int;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function entity() {
+        return $this->entity;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function attachedInt() {
+        return $this->attached_int;
+    }
+}
