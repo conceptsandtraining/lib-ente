64259b1bb697eb727614a81a410f24c4982f60cb
Richard Klees <richard.klees@concepts-and-training.de>
2017-04-12 17:12:51 +0200

    added new component AttachString for Testing.

---
+++ src/Simple/AttachStringMemory.php
@@ -0,0 +1,46 @@
+<?php
+/******************************************************************************
+ * An entity component framework for PHP.
+ *
+ * Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de>
+ *
+ * This software is licensed under GPLv3. You should have received a copy of
+ * the license along with the code.
+ */
+
+namespace CaT\Ente\Simple;
+
+/**
+ * In memory implementation of AttachString.
+ */
+class AttachStringMemory implements \CaT\Ente\Simple\AttachString {
+    /**
+     * @var \CaT\Ente\Component
+     */
+    private $entity;
+
+    /**
+     * @var string
+     */
+    private $attached_string;
+
+    public function __construct(Entity $entity, $attached_string) {
+        assert('is_string($attached_string)');
+        $this->entity = $entity;
+        $this->attached_string = $attached_string;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function entity() {
+        return $this->entity;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function attachedString() {
+        return $this->attached_string;
+    }
+}
