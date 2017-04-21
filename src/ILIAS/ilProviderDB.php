afde2401972788e6e4050ce5630caf22d30efaeb
Richard Klees <richard.klees@concepts-and-training.de>
2017-04-21 11:13:24 +0200

    implemented ilProviderDB::create

--- src/ILIAS/ilProviderDB.php
+++ src/ILIAS/ilProviderDB.php
@@ -17,6 +17,9 @@ class ilProviderDB implements ProviderDB {
     const PROVIDER_TABLE = "ente_providers";
     const COMPONENT_TABLE = "ente_provider_components";
 
+    const CLASS_NAME_LENGTH = 128;
+    const PATH_LENGTH = 1024;
+
     /**
      * @var \ilDBInterface
      */
@@ -29,7 +32,48 @@ class ilProviderDB implements ProviderDB {
     /**
      * @inheritdocs
      */
-    public function create(\ilObject $owner, $obj_type, $class_name, $include_path) {
+    public function create(\ilObject $owner, $object_type, $class_name, $include_path) {
+        assert('is_string($object_type)');
+        assert('is_string($class_name)');
+        assert('is_string($include_path)');
+        if (strlen($object_type) > 4) {
+            throw new \LogicException("Expected object type '$object_type' to have four or less chars.");
+        }
+        if (strlen($class_name) > ilProviderDB::CLASS_NAME_LENGTH) {
+            throw new \LogicException(
+                        "Expected class name '$class_name' to have at most "
+                        .ilProviderDB::CLASS_NAME_LENGTH." chars.");
+        }
+        if (strlen($include_path) > ilProviderDB::PATH_LENGTH) {
+            throw new \LogicException(
+                        "Expected include path '$include_path' to have at most "
+                        .ilProviderDB::PATH_LENGTH." chars.");
+        }
+
+        $id= $this->ilDB->nextId(ilProviderDB::PROVIDER_TABLE);
+        $this->ilDB->insert(ilProviderDB::PROVIDER_TABLE,
+            [ "id" => ["integer", $id]
+            , "owner" => ["integer", $owner->getId()]
+            , "object_type" => ["string", $object_type]
+            , "class_name" => ["string", $class_name]
+            , "include_path" => ["string", $include_path]
+            ]);
+
+        $unbound_provider = $this->buildUnboundProvider($id, $owner, $class_name, $class_name, $include_path);
+
+        foreach ($unbound_provider->componentTypes() as $component_type) {
+            if (strlen($component_type) > ilProviderDB::CLASS_NAME_LENGTH) {
+                throw new \LogicException(
+                            "Expected component type '$class_name' to have at most "
+                            .ilProviderDB::CLASS_NAME_LENGTH." chars.");
+            }
+            $this->ilDB->insert(ilProviderDB::COMPONENT_TABLE,
+                [ "id" => ["integer", $id]
+                , "component_type" => ["string", $component_type]
+                ]);
+        }
+
+        return $unbound_provider;
     }
 
     /**
@@ -61,15 +105,45 @@ class ilProviderDB implements ProviderDB {
                 [ "id" => ["type" => "integer", "length" => 4, "notnull" => true]
                 , "owner" => ["type" => "integer", "length" => 4, "notnull" => true]
                 , "object_type" => ["type" => "string", "length" => 4, "notnull" => true]
-                , "class_name" => ["type" => "string", "length" => 64, "notnull" => true]
-                , "include_path" => ["type" => "string", "length" => 1024, "notnull" => true]
+                , "class_name" => ["type" => "string", "length" => ilProviderDB::CLASS_NAME_LENGTH, "notnull" => true]
+                , "include_path" => ["type" => "string", "length" => ilProviderDB::PATH_LENGTH, "notnull" => true]
                 ]);
+            $this->ilDB->addPrimaryKey(ilProviderDB::PROVIDER_TABLE, ["id"]);
         }
         if (!$this->ilDB->tableExists(ilProviderDB::COMPONENT_TABLE)) {
             $this->ilDB->createTable(ilProviderDB::COMPONENT_TABLE,
                 [ "id" => ["type" => "integer", "length" => 4, "notnull" => true]
-                , "component_type" => ["type" => "string", "length" => 64, "notnull" => true]
+                , "component_type" => ["type" => "string", "length" => ilProviderDB::CLASS_NAME_LENGTH, "notnull" => true]
                 ]);
+            $this->ilDB->addPrimaryKey(ilProviderDB::COMPONENT_TABLE, ["id", "component_type"]);
         }
+    }
+
+    /**
+     * Create an unbound provider.
+     *
+     * @param   \ilObject   $owner
+     * @param   string      $object_type
+     * @param   string      $class_name
+     * @param   string      $include_path
+     * @return  UnboundProvider
+     */
+    protected function buildUnboundProvider($id, \ilObject $owner, $object_type, $class_name, $include_path) {
+        assert('is_int($id)');
+        assert('is_string($object_type)');
+        assert('is_string($class_name)');
+        assert('is_string($include_path)');
+        assert('file_exists($include_path)');
+
+        require_once($include_path);
+
+        assert('class_exists($class_name)');
+
+        if (!is_subclass_of($class_name, UnboundProvider::class)) {
+            throw new UnexpectedValueException(
+                        "Class '$class_name' does not extend UnboundProvider.");
+        }
+
+        return new $class_name($id, $owner, $object_type);
     }
 }
--- tests/ILIAS/ilProviderDBTest.php
+++ tests/ILIAS/ilProviderDBTest.php
@@ -9,6 +9,8 @@
  */
 
 use CaT\Ente\ILIAS\ilProviderDB;
+use CaT\Ente\Simple\AttachString;
+use CaT\Ente\Simple\AttachInt;
 
 if (!class_exists("ilObject")) {
     require_once(__DIR__."/ilObject.php");
@@ -19,23 +21,27 @@ if (!interface_exists("ilDBInterface")) {
 }
 
 class ILIAS_ilProviderDBTest extends PHPUnit_Framework_TestCase {
-    public function test_createTables() {
-        $il_db = $this
+    protected function il_db_mock() {
+        return $this
             ->getMockBuilder(\ilDBInterface::class)
             ->setMethods(["nextId","createTable","addPrimaryKey","createSequence",
                           "tableExists","addIndex","query","insert","fetchAssoc","quote"])
             ->getMock();
+    }
+
+    public function test_createTables() {
+        $il_db = $this->il_db_mock();
 
         $provider_table =
             [ "id" => ["type" => "integer", "length" => 4, "notnull" => true]
             , "owner" => ["type" => "integer", "length" => 4, "notnull" => true]
             , "object_type" => ["type" => "string", "length" => 4, "notnull" => true]
-            , "class_name" => ["type" => "string", "length" => 64, "notnull" => true]
-            , "include_path" => ["type" => "string", "length" => 1024, "notnull" => true]
+            , "class_name" => ["type" => "string", "length" => ilProviderDB::CLASS_NAME_LENGTH, "notnull" => true]
+            , "include_path" => ["type" => "string", "length" => ilProviderDB::PATH_LENGTH, "notnull" => true]
             ];
         $component_table =
             [ "id" => ["type" => "integer", "length" => 4, "notnull" => true]
-            , "component_type" => ["type" => "string", "length" => 64, "notnull" => true]
+            , "component_type" => ["type" => "string", "length" => ilProviderDB::CLASS_NAME_LENGTH, "notnull" => true]
             ];
 
         $il_db
@@ -50,8 +56,71 @@ class ILIAS_ilProviderDBTest extends PHPUnit_Framework_TestCase {
             ->method("tableExists")
             ->withConsecutive([ilProviderDB::PROVIDER_TABLE], [ilProviderDB::COMPONENT_TABLE])
             ->will($this->onConsecutiveCalls(false, false));
+
+        $il_db
+            ->expects($this->exactly(2))
+            ->method("addPrimaryKey")
+            ->withConsecutive(
+                [ilProviderDB::PROVIDER_TABLE, ["id"]],
+                [ilProviderDB::COMPONENT_TABLE, ["id", "component_type"]]);
    
         $db = new ilProviderDB($il_db);
         $db->createTables();
+    }
+
+    public function test_create() {
+        $il_db = $this->il_db_mock();
+
+        $owner = $this
+            ->getMockBuilder(\ilObject::class)
+            ->setMethods(["getId"])
+            ->getMock();
+
+        $owner_id = 42;
+        $owner
+            ->method("getId")
+            ->willReturn($owner_id);
+
+        $new_provider_id = 23;
+        $object_type = "crs";
+        $class_name = Test_UnboundProvider::class;
+        $include_path = __DIR__."/UnboundProviderTest.php";
+
+        $insert_provider =
+            [ "id" => ["integer", $new_provider_id]
+            , "owner" => ["integer", $owner_id]
+            , "object_type" => ["string", $object_type]
+            , "class_name" => ["string", $class_name]
+            , "include_path" => ["string", $include_path]
+            ];
+
+        $insert_component_1 =
+            [ "id" => ["integer", $new_provider_id]
+            , "component_type" => ["string", AttachString::class]
+            ];
+
+        $insert_component_2 =
+            [ "id" => ["integer", $new_provider_id]
+            , "component_type" => ["string", AttachInt::class]
+            ];
+
+        $il_db
+            ->expects($this->exactly(3))
+            ->method("insert")
+            ->withConsecutive(
+                [ilProviderDB::PROVIDER_TABLE, $insert_provider],
+                [ilProviderDB::COMPONENT_TABLE, $insert_component_1],
+                [ilProviderDB::COMPONENT_TABLE, $insert_component_2]);
+
+        $il_db
+            ->expects($this->once())
+            ->method("nextId")
+            ->with(ilProviderDB::PROVIDER_TABLE)
+            ->willReturn($new_provider_id);
+
+        $db = new ilProviderDB($il_db);
+        $unbound_provider = $db->create($owner, $object_type, $class_name, $include_path);
+
+        $this->assertInstanceOf(Test_UnboundProvider::class, $unbound_provider);
     }
 }
