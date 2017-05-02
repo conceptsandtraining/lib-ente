b23cb64737d697d06879a815cb50cbac91b162d7
Richard Klees <richard.klees@concepts-and-training.de>
2017-05-02 13:06:04 +0200

    added objId to settings of ComponentProvider plugin.

--- example/ComponentProviderExample/classes/Settings/ComponentProviderExample.php
+++ example/ComponentProviderExample/classes/Settings/ComponentProviderExample.php
@@ -7,13 +7,29 @@ namespace CaT\Plugins\ComponentProviderExample\Settings;
  */
 class ComponentProviderExample {
     /**
+     * @var int
+     */
+    protected $obj_id;
+
+    /**
      * @var string[]
      */
     protected $provided_strings;
 
-    public function __construct(array $provided_strings) {
+    public function __construct($obj_id, array $provided_strings) {
+        assert('is_int($obj_id)');
         assert('array_sum(array_map("is_string", $provided_strings)) == count($provided_strings)');
+        $this->obj_id = $obj_id;
         $this->provided_strings = $provided_strings;
+    }
+
+    /**
+     * Get the id of the object this is belonging to.
+     *
+     * @return  int
+     */
+    public function objId() {
+        return $this->obj_id;
     }
 
     /**
