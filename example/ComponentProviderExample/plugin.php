8c55b39e8f85479d8e96baa91ef6f76c090629fd
Richard Klees <richard.klees@concepts-and-training.de>
2017-05-02 12:53:48 +0200

    added plugin.php

---
+++ example/ComponentProviderExample/plugin.php
@@ -0,0 +1,16 @@
+<?php
+// alphanumerical ID of the plugin; never change this
+// up to 4 letters
+$id = "xlep";
+
+// code version; must be changed for all code changes
+$version = "0.0.1";
+
+// ilias min and max version; must always reflect the versions that should
+// run with the plugin
+$ilias_min_version = "";
+$ilias_max_version = "";
+
+// optional, but useful: Add one or more responsible persons and a contact email
+$responsible = "Richard Klees";
+$responsible_mail = "richard.klees@concepts-and-training.de";
