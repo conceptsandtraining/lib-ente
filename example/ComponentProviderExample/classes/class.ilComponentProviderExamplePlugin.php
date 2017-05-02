8f607545369b7b9eb8bb65552062761ac000a934
Richard Klees <richard.klees@concepts-and-training.de>
2017-05-02 15:49:26 +0200

    edit provided string at ComponentProvider example plugin.

--- example/ComponentProviderExample/classes/class.ilComponentProviderExamplePlugin.php
+++ example/ComponentProviderExample/classes/class.ilComponentProviderExamplePlugin.php
@@ -1,10 +1,29 @@
 <?php
 include_once("./Services/Repository/classes/class.ilRepositoryObjectPlugin.php");
+require_once(__DIR__."/../vendor/autoload.php");
 
 /**
  * Plugin base class. Keeps all information the plugin needs
  */
 class ilComponentProviderExamplePlugin extends ilRepositoryObjectPlugin {
+    /**
+     * @var \ilDBInterface
+     */
+    protected $ilDB;
+
+    /**
+     * @var \CaT\Plugins\ComponentProviderExamplePlugin\Settings\DB|null
+     */
+    protected $settings_db = null;
+
+    /**
+     * Object initialisation. Overwritten from ilPlugin.
+     */
+    protected function init() {
+        global $DIC;
+        $this->ilDB = $DIC->database();
+    }
+
 	/**
 	 * Get the name of the Plugin
 	 *
@@ -19,4 +38,16 @@ class ilComponentProviderExamplePlugin extends ilRepositoryObjectPlugin {
 	 */
 	protected function uninstallCustom() {
 	}
+
+    /**
+     * Get the database for settings.
+     *
+     * @return \CaT\Plugins\ComponentProviderExample\Settings\DB
+     */
+    public function settingsDB() {
+        if ($this->settings_db === null) {
+            $this->settings_db = new \CaT\Plugins\ComponentProviderExample\Settings\ilDB($this->ilDB);
+        }
+        return $this->settings_db;
+    }
 }
--- example/ComponentProviderExample/classes/class.ilObjComponentProviderExampleGUI.php
+++ example/ComponentProviderExample/classes/class.ilObjComponentProviderExampleGUI.php
@@ -1,6 +1,9 @@
 <?php
 
-include_once("./Services/Repository/classes/class.ilObjectPluginGUI.php");
+require_once("./Services/Repository/classes/class.ilObjectPluginGUI.php");
+require_once("./Services/Form/classes/class.ilPropertyFormGUI.php");
+require_once("./Services/Form/classes/class.ilTextInputGUI.php");
+
 
 /**
  * Plugin object GUI class. Baseclass for all GUI action in ILIAS
@@ -9,10 +12,26 @@ include_once("./Services/Repository/classes/class.ilObjectPluginGUI.php");
  * @ilCtrl_Calls ilObjComponentProviderExampleGUI: ilPermissionGUI, ilInfoScreenGUI, ilObjectCopyGUI, ilCommonActionDispatcherGUI
  */
 class ilObjComponentProviderExampleGUI  extends ilObjectPluginGUI {
+    const VALUES_FIELD_NAME = "values";
+    const SAVE_CMD = "saveForm";
+
+    /**
+     * @var \ilTemplate
+     */
+    protected $ilTemplate;
+
+    /**
+     * @var \ilCtrl
+     */
+    protected $ilCtrl;
+
 	/**
 	 * Called after parent constructor. It's possible to define some plugin special values
 	 */
 	protected function afterConstructor() {
+        global $DIC;
+        $this->ilTemplate = $DIC->ui()->mainTemplate();
+        $this->ilCtrl = $DIC->ctrl();
 	}
 
 	/**
@@ -27,21 +46,61 @@ class ilObjComponentProviderExampleGUI  extends ilObjectPluginGUI {
 	*/
 	function performCommand($cmd) {
 		switch ($cmd) {
+            case self::SAVE_CMD:
+                $this->saveForm();
+            case "showContent":
+                $this->ilTemplate->setContent($this->showContent());
+                break;
 			default:
+                throw new \InvalidArgumentException("Unknown Command: '$cmd'");
 		}
 	}
+
+    /**
+     * Save values provided from form.
+     */
+    protected function saveForm() {
+        $db = $this->plugin->settingsDB();
+        $settings = $db->getFor($this->object->getId());
+        $settings = $settings->withProvidedStrings($_POST[self::VALUES_FIELD_NAME]);
+        $db->update($settings);
+    }
+
+    /**
+     * Show the edit form.
+     *
+     * @return string
+     */
+    public function showContent() {
+        $db = $this->plugin->settingsDB();
+        $settings = $db->getFor($this->object->getId());
+
+        $form = new \ilPropertyFormGUI();
+        $form->setTitle($this->plugin->txt("settings_form_title"));
+        $form->setFormAction($this->ilCtrl->getFormAction($this));
+        $form->addCommandButton(self::SAVE_CMD, $this->txt("save"));
+
+        $input = new \ilTextInputGUI($this->plugin->txt("values"), self::VALUES_FIELD_NAME);
+        $input->setMulti(true);
+        $input->setMaxLength(64);
+        $input->setValue($settings->providedStrings());
+
+        $form->addItem($input);
+
+        return $form->getHTML();
+    }
 
 	/**
 	* After object has been created -> jump to this command
 	*/
 	function getAfterCreationCmd() {
-		return "";
+		return "showContent";
 	}
 
 	/**
 	* Get standard command
 	*/
 	function getStandardCmd() {
-		return "";
+		return "showContent";
 	}
 }
