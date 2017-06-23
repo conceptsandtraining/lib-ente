dc55a97378a60c960e526e6a9b4fdd427910e9ab
Richard Klees <richard.klees@concepts-and-training.de>
2017-06-23 14:43:50 +0200

    implemented missing methods in ilProviderDB by using statics =(

--- lib/src/ILIAS/ilProviderDB.php
+++ lib/src/ILIAS/ilProviderDB.php
@@ -295,6 +295,7 @@ class ilProviderDB implements ProviderDB {
      * @return  \ilObject
      */
     protected function buildObjectByRefId($ref_id) {
+        ilObjectFactory::getInstanceByRefId($ref_id);
     }
 
     /**
@@ -305,6 +306,7 @@ class ilProviderDB implements ProviderDB {
      * @return  \ilObject
      */
     protected function buildObjectByObjId($ref_id) {
+        ilObjectFactory::getInstanceByObjId($ref_id);
     }
 
     /**
@@ -314,5 +316,6 @@ class ilProviderDB implements ProviderDB {
      * @return  int[]
      */
     protected function getAllReferenceIdsFor($obj_id) {
+        ilObject::_getAllReferences($obj_id);
     }
 }
