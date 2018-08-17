43cdafde94e2cf138287fcf665e3bcb98e6089cd
Richard Klees <richard.klees@concepts-and-training.de>
2018-08-17 14:39:34 +0200

    implemented cache over ilias

--- src/ILIAS/Cache.php
+++ src/ILIAS/Cache.php
@@ -2,7 +2,7 @@
 /******************************************************************************
  * An entity component framework for PHP.
  *
- * Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de>
+ * Copyright (c) 2018 Richard Klees <richard.klees@concepts-and-training.de>
  *
  * This software is licensed under GPLv3. You should have received a copy of
  * the license along with the code.
